f6876a859dcd227e7c40d624d43b3e98197bfc9a
Pinggou-C <milomatiscornelissen@gmail.com>
2022-03-22 12:36:07 +0100

    pinggou

--- Globals/time/pick-pause-reverse_module.gd
+++ Globals/time/pick-pause-reverse_module.gd
@@ -1,5 +1,13 @@
 extends Spatial
 
+var positions = []
+var oldposs
+var velocities = []
+var newvel
+var oldstate
+var cancel_reverse = false
+
+
 var parent = "rigid"
 var picked_up = false
 var state = "normal"
@@ -19,6 +27,8 @@ func _ready():
 		set_physics_process(true)
 	if type == "both":
 		add_to_group("Pause-&reverse-able")
+		add_to_group("reverseable")
+		add_to_group("pauseable")
 		set_physics_process(true)
 	if weighted:
 		add_to_group("weighted")
@@ -34,28 +44,65 @@ func collision_layer():
 
 
 func _physics_process(delta):
-	pass
+	var parpos = Vector3(stepify(get_parent().get_global_transform().origin.x, 0.01), stepify(get_parent().get_global_transform().origin.y, 0.01), stepify(get_parent().get_global_transform().origin.z, 0.01))
+	if state == "normal" || picked_up:
+		if oldposs != parpos:
+			positions.push_front(parpos)
+			if cancel_reverse == true:
+				cancel_reverse = false
+			if parent == "rigid":
+				velocities.push_front(get_parent().linear_velocity)
+	elif state == "reversed":
+		if !picked_up && !cancel_reverse:
+			get_parent().global_transform.origin = positions[0]
+			positions.remove(0)
+			if velocities.size() > 0:
+				newvel = velocities[0]
+			if positions.size() == velocities.size() -1:
+				velocities.remove(0)
+			if positions.size() == 0:
+				cancel_reverse = true
+	if oldstate == "reversed" && state != "reversed":
+		if state == "normal":
+			get_parent().linear_velocity = newvel
+		elif state == "paused":
+			old_velocity = newvel
+			newvel = null
+	oldstate = state
+	oldposs = parpos
 
 func pause():
+	get_parent().get_child(2).get_child(0).visible = true
+	get_parent().get_child(2).get_child(1).visible = false
 	if picked_up == false:
-		old_velocity = get_parent().linear_velocity
-		var body := rigid_to_kinem(get_parent())
-		collision_layer()
-		parent = "kinem"
+		if parent == "rigid":
+			old_velocity = get_parent().linear_velocity
+			var body := rigid_to_kinem(get_parent())
+			collision_layer()
+			parent = "kinem"
 	state = "paused"
-
+	
 func Continue():
+	get_parent().get_child(2).get_child(0).visible = false
+	get_parent().get_child(2).get_child(1).visible = false
 	if picked_up == false:
-		var body := kinem_to_rigid(get_parent())
+		if parent == "kinem":
+			var body := kinem_to_rigid(get_parent())
 		$Tween.interpolate_property(get_parent(), "linear_velocity", Vector3.ZERO, old_velocity, 0.2, Tween.TRANS_LINEAR, Tween.EASE_IN)
 		old_velocity = Vector3.ZERO
 		collision_layer()
 		parent = "rigid"
 	state = "normal"
 
-func Reverse():
+func reverse():
+	get_parent().get_child(2).get_child(0).visible = false
+	get_parent().get_child(2).get_child(1).visible = true
 	if picked_up == false:
-		pass
+		if parent == "rigid":
+			var body := rigid_to_kinem(get_parent())
+			collision_layer()
+			parent = "kinem"
+	state = "reversed"
 
 
 func picked_up():
@@ -70,10 +117,20 @@ func drop(vel):
 	var body = get_parent()
 	picked_up = false
 	if state == "paused":
-		pass
+		if parent == "rigid":
+			rigid_to_kinem(get_parent())
+			get_parent().get_child(2).get_child(0).visible = true
+			get_parent().get_child(2).get_child(1).visible = false
+			parent = "kinem"
 	elif state == "reversed":
-		pass
+		if parent == "rigid":
+			get_parent().get_child(2).get_child(0).visible = false
+			get_parent().get_child(2).get_child(1).visible = true
+			rigid_to_kinem(get_parent())
+			parent = "kinem"
 	elif state == "normal":
+		get_parent().get_child(2).get_child(0).visible = false
+		get_parent().get_child(2).get_child(1).visible = false
 		body = kinem_to_rigid(get_parent())
 		get_parent().linear_velocity = vel
 		parent = "rigid"
